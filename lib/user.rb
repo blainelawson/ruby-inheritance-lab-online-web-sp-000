d6c5b39d116e0e8ced6dee8e5d5c8cb3fe0e4d55
Blaine Lawson <blaine.lawson@gmail.com>
2019-03-18 21:12:50 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -3,11 +3,11 @@ class User
 
   def first_name=(name)
     @first_name = name
-    first_name
+    @first_name
   end
 
   def last_name=(name)
     @last_name = name
-    last_name
+    @last_name
   end
 end
