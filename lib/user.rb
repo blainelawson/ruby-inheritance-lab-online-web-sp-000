bd1f997b07285ae1cb59689ef480609e79d86954
Blaine Lawson <blaine.lawson@gmail.com>
2019-03-18 21:12:41 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -2,12 +2,12 @@ class User
   attr_accessor :first_name, :last_name
 
   def first_name=(name)
-    @first_name = first_name
+    @first_name = name
     first_name
   end
 
   def last_name=(name)
-    @last_name = last_name
+    @last_name = name
     last_name
   end
 end
