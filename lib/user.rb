f3ea416a150f6a6cb4eff4e53c2fd1398a9ae60e
Blaine Lawson <blaine.lawson@gmail.com>
2019-03-18 21:11:38 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,9 +1,11 @@
 class User
   attr_accessor :first_name, :last_name
 
-  def initialize(first_name, last_name)
+  def first_name=(name)
     @first_name = first_name
-    @last_name = last_name
+  end
 
+  def last_name=(name)
+    @last_name = last_name
   end
 end
